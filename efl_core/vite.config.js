7838d8ac8f5f20805008bdbeac9a4d346c612ed0
Rb-tech-byte <bernadorenatus39@gmail.com>
2026-01-06 09:21:55 +0300

    Configure Vite for nested deployment (efl_core inside public_html)

--- efl_core/vite.config.js
+++ efl_core/vite.config.js
@@ -7,12 +7,12 @@ export default defineConfig({
         laravel({
             input: 'resources/js/app.tsx',
             refresh: true,
-            publicDirectory: '../public_html',
+            publicDirectory: '../',
         }),
         react(),
     ],
     build: {
-        outDir: '../public_html/build',
+        outDir: '../build',
         emptyOutDir: true,
         chunkSizeWarningLimit: 1500,
         rollupOptions: {
